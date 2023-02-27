af6d2d32397202c51ddbfd5a0cd77e68644173b5
Listopaddon <67251998+Listopaddon@users.noreply.github.com>
2023-02-27 11:18:53 +0300

    Add project files.

---
+++ Lesson32/MyCollection.cs
@@ -0,0 +1,52 @@
+﻿//Создать собственную коллекцию реализующий IEnumerable интерфейс,
+//который принимает в себя объекты и при перечислении выдает сумму со следующим элементом. 
+
+using System.Collections;
+
+namespace Lesson32
+{
+    internal class MyCollection : IEnumerable, IEnumerator
+    {
+        private List<int> myCollection = new List<int>();
+        private int index = -1;
+
+        public int Index { get { return index; } }
+
+        public object Current => myCollection[index];
+
+        public void Add(int i)
+        {
+            myCollection.Add(i);
+        }
+
+        public void AddCollection(List<int> numbers)
+        {
+            myCollection = numbers;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < myCollection.Count - 1; i++)
+            {
+                yield return myCollection[i] + myCollection[i + 1];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (index == myCollection.Count - 1)
+            {
+                return false;
+            }
+            index++;
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+
+    }
+}
---
+++ Lesson32/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lesson32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+
+            MyCollection myCollection = new MyCollection();
+            myCollection.AddCollection(numbers);
+
+            foreach (int i in myCollection)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
